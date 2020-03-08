512f50036107f1439ec45cf8c000ce22c3bf11e1
SanMasood <sanamasood.giki@gmail.com>
2020-03-08 23:32:43 +0000

    Bill calculated time to pay now

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 function calculateBill(price, vat, tip){
   
+  if (typeof price !== 'number' || typeof vat !== 'number' || typeof tip !== 'number' ){
+  return 'error';
+  }
+  
   return (`£${price + (price * vat / 100) + tip}`);
+  
 }
 
 
